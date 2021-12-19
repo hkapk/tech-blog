cc6af31afbfe36a46d170d6808555d5811db3717
Hank Kapka <hankkapka@gmail.com>
2021-12-19 15:42:11 -0600

    connected to express and linked database for login, create account

--- config/connection.js
+++ config/connection.js
@@ -10,6 +10,8 @@ if (process.env.JAWSDB_URL) {
 } else {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
+    user: 'root',
+    password: 'password',
     dialect: 'mysql',
     port: 3306
   });
